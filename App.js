7eb67f9ab731a49089e5d47166f38bc970191e2b
David Andrews <dandrewsuk82@gmail.com>
2020-12-08 12:21:48 +0000

    Autogit: 08-12-2020 12:21

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
+import * as Location from "expo-location";
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const status = await Location.requestPermissionsAsync();
+      Location.watchPositionAsync({}, (a) => {
+        console.log(a);
+      });
+    })();
+  }, []);
   return (
     <View style={{ flex: 1 }}>
       <Navbar />
