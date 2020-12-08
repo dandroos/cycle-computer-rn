c8b60ecd11955cab10cbb8366431ac2516b3d624
David Andrews <dandrewsuk82@gmail.com>
2020-12-08 13:52:03 +0000

    Autogit: 08-12-2020 13:52

--- App.js
+++ App.js
@@ -4,30 +4,32 @@ import { View } from "react-native";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 import * as Location from "expo-location";
-import { getPreciseDistance } from "geolib";
+import { getDistance } from "geolib";
 
 export default function App() {
   const [lastPosition, setLastPosition] = useState(null);
+  const [speed, setSpeed] = useState(0);
 
   useEffect(() => {
     (async () => {
       const status = await Location.requestPermissionsAsync();
       Location.watchPositionAsync({ accuracy: 6 }, (a) => {
-        console.log(a);
         if (lastPosition) {
-          console.log("helloooooooo!");
-          console.log(
-            getPreciseDistance(
-              {
-                latitude: lastPosition.coords.latitude,
-                longitude: lastPosition.coords.longitude,
-              },
-              {
-                latitude: a.coords.latitude,
-                longitude: a.coords.longitude,
-              }
-            )
+          const distance = getDistance(
+            {
+              latitude: lastPosition.coords.latitude,
+              longitude: lastPosition.coords.longitude,
+            },
+            {
+              latitude: a.coords.latitude,
+              longitude: a.coords.longitude,
+            }
           );
+          console.log("yoohoooooo");
+          const res = Math.abs(a.timestamp / lastPosition.timestamp) / 1000;
+          const secondsSinceLastUpdate = res % 60;
+
+          console.log(secondsSinceLastUpdate);
         }
         setLastPosition(a);
       });
